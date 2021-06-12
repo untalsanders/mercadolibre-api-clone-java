78ec91287424756c6f685531bac070485e349d52
Sanders Gutiérrez <ing.sanders@gmail.com>
2021-06-12 17:35:04 -0300

    First commit
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

---
+++ src/test/java/com/sandersgutierrez/cotomarket/CotoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandersgutierrez.cotomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotoMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
