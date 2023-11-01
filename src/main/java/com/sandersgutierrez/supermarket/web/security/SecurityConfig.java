33e184740420949dbb290c834f953db484e5247e
Sanders Gutiérrez <ing.sanders@gmail.com>
2023-10-31 21:01:19 -0300

    chore: enable h2-console

--- src/main/java/com/sandersgutierrez/supermarket/web/security/SecurityConfig.java
+++ src/main/java/com/sandersgutierrez/supermarket/web/security/SecurityConfig.java
@@ -27,7 +27,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/**/authenticate").permitAll()
                 .anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.headers().frameOptions().sameOrigin();
